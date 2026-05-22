5dfdde4de32547c4623d84e76738889ac8d41c85
gmoilu951-cyber <gmoilu951@gmail.com>
2026-05-22 22:47:43 +0600

    Update Footer.tsx

--- app/sections/Footer.tsx
+++ app/sections/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (  
     <footer className="text-sm text-gray-600 flex flex-col justify-center items-center bg-gray-200 py-3 border-t border-gray-300">
-      <p>© {new Date().getFullYear()} Gaeuly. All Rights Reserved.</p>
+      <p>© {new Date().getFullYear()} Nil. All Rights Reserved.</p>
     </footer>
   );
 }
