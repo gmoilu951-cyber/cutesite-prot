74529d78560f4e98234834ea476e8a9ce3ea61db
gmoilu951-cyber <gmoilu951@gmail.com>
2026-05-22 22:42:24 +0600

    Update About.tsx

--- app/sections/About.tsx
+++ app/sections/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
         <div className="max-w-2xl mx-auto px-4 flex flex-col items-center pb-32">
             <h1 className="font-jersey font-bold text-5xl py-12">About Me</h1>
             <div className="flex flex-col justify-center leading-relaxed text-center font-pixelify gap-5">
-                <p>Hi, I’m gaeuly! I’m a not a dev but like coding hmm is it okay?</p>
+                <p>Hi, I’m Nil! I’m a not a dev but like coding hmm is it okay?</p>
                 <p>I’m passionate about webs app, bot discord, and minecraft management server </p>
                 <p>Find more about me in my dashboard!</p>
             </div>
